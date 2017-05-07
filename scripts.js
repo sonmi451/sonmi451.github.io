aae4cceab3d9e0a714c888a5e456930d2331bc37
Aisha McLean <aishamclean451@gmail.com>
2017-05-07 20:31:01 +0100

    embedded video

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var HourNow = new Date();
+/*
+  $(document).ready(function() {
+    $("#AwakeTimeCheck").innerHTML();
+    document.getElementById("AwakeTimeCheck").innerHTML()"Mouses");
+    document.getElementById("AwakeTimeCheck").innerHTML(HourNow);
+  }
+
+
+
+    if (AwakeTime >= 20) {
+      AwakeTimeCheck = "Mouse awake time!";
+    } else {
+    AwakeTimeCheck = "Mouse asleep time.";
+    }
